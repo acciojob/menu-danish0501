5d3f8615af1678706cfc5ca9e2ea6300a87baca7
danish0501 <alidanish373@gmail.com>
2023-09-13 12:07:10 +0530

    submitting code-Wed Sep 13 2023 12:07:08 GMT+0530 (India Standard Time)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import MenuItems from "./components/MenuItems";
+
+const App = () => {
+    return(
+        <div>
+            <MenuItems/>
+        </div>
+    )
+}
+
+export default App;
+
+
+
---
+++ src/components/MenuItems.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+
+const dishesData = [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: './images/item-1.jpeg',
+        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+      },
+      {
+        id: 2,
+        title: 'diner double',
+        category: 'lunch',
+        price: 13.99,
+        img: './images/item-2.jpeg',
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+      },
+      {
+        id: 3,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 6.99,
+        img: './images/item-3.jpeg',
+        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+      },
+      {
+        id: 4,
+        title: 'country delight',
+        category: 'breakfast',
+        price: 20.99,
+        img: './images/item-4.jpeg',
+        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+      },
+      {
+        id: 5,
+        title: 'egg attack',
+        category: 'lunch',
+        price: 22.99,
+        img: './images/item-5.jpeg',
+        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+      },
+      {
+        id: 6,
+        title: 'oreo dream',
+        category: 'shakes',
+        price: 18.99,
+        img: './images/item-6.jpeg',
+        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+      },
+      {
+        id: 7,
+        title: 'bacon overflow',
+        category: 'breakfast',
+        price: 8.99,
+        img: './images/item-7.jpeg',
+        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+      },
+      {
+        id: 8,
+        title: 'american classic',
+        category: 'lunch',
+        price: 12.99,
+        img: './images/item-8.jpeg',
+        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+      },
+      {
+        id: 9,
+        title: 'quarantine buddy',
+        category: 'shakes',
+        price: 16.99,
+        img: './images/item-9.jpeg',
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+      },
+];
+
+function App() {
+  const [category, setCategory] = useState('all');
+  const [dishes, setDishes] = useState(dishesData);
+
+  const filterDishes = (category) => {
+    if (category === 'all') {
+      setDishes(dishesData);
+    } else {
+      const filteredDishes = dishesData.filter((dish) => dish.category === category);
+      setDishes(filteredDishes);
+    }
+    setCategory(category);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dish Reviews</h1>
+      <div className="categories">
+        <button onClick={() => filterDishes('all')}>All Dishes</button>
+        <button onClick={() => filterDishes('breakfast')}>Breakfast</button>
+        <button onClick={() => filterDishes('lunch')}>Lunch</button>
+        <button onClick={() => filterDishes('shakes')}>Shakes</button>
+        {/* Add more category buttons as needed */}
+      </div>
+      <div className="dishes">
+        {dishes.map((dish) => (
+          <div key={dish.id} className="dish-card">
+            <img src={dish.img} alt={dish.title} />
+            <h2>{dish.title}</h2>
+            <p>{dish.desc}</p>
+            <p>${dish.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
